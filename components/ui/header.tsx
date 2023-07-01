a3ee94cc6447320e57cfb6848ff873cb31c7862f
YahavMesika <yahavmesika@gmail.com>
2023-07-01 12:48:49 +0300

    Update header.tsx

--- components/ui/header.tsx
+++ components/ui/header.tsx
@@ -1,5 +1,98 @@
-// import Link from "next/link";
-// import MobileMenu from "./mobile-menu";
+// // import Link from "next/link";
+// // import MobileMenu from "./mobile-menu";
+
+// // export default function Header() {
+// //   return (
+// //     <header className="absolute w-full z-30">
+// //       <div className="max-w-6xl mx-auto px-4 sm:px-6">
+// //         <div className="flex items-center justify-between h-20">
+// //           {/* Site branding */}
+// //           <div className="shrink-0 mr-2 mb-2">
+// //             {/* Logo */}
+// //             <p>Yahav Mesika </p>
+// //           </div>
+
+// //           <div className="md:flex md:items-center md:justify-between">
+// //             {/* Social links */}
+// //             <ul className="flex  md:order-1 md:ml-4 md:mb-0">
+// //               <p className="mr-2"> 054-2522677</p>
+// //               <p>Social:</p>
+// //               <li className="ml-2">
+// //                 <Link
+// //                   href="https://www.linkedin.com/in/yahav-mesika/"
+// //                   className="flex justify-center items-center text-purple-600 bg-gray-800 hover:text-gray-100 hover:bg-purple-600 rounded-full transition duration-150 ease-in-out"
+// //                   aria-label="LinkedIn"
+// //                 >
+// //                   <svg
+// //                     className="w-8 h-8 fill-current"
+// //                     viewBox="0 0 32 32"
+// //                     xmlns="http://www.w3.org/2000/svg"
+// //                   >
+// //                     <path d="M16 8.2c-4.4 0-8 3.6-8 8 0 3.5 2.3 6.5 5.5 7.6.4.1.5-.2.5-.4V22c-2.2.5-2.7-1-2.7-1-.4-.9-.9-1.2-.9-1.2-.7-.5.1-.5.1-.5.8.1 1.2.8 1.2.8.7 1.3 1.9.9 2.3.7.1-.5.3-.9.5-1.1-1.8-.2-3.6-.9-3.6-4 0-.9.3-1.6.8-2.1-.1-.2-.4-1 .1-2.1 0 0 .7-.2 2.2.8.6-.2 1.3-.3 2-.3s1.4.1 2 .3c1.5-1 2.2-.8 2.2-.8.4 1.1.2 1.9.1 2.1.5.6.8 1.3.8 2.1 0 3.1-1.9 3.7-3.7 3.9.3.4.6.9.6 1.6v2.2c0 .2.1.5.6.4 3.2-1.1 5.5-4.1 5.5-7.6-.1-4.4-3.7-8-8.1-8z" />
+// //                   </svg>
+// //                 </Link>
+// //               </li>
+// //             </ul>
+// //           </div>
+// //         </div>
+// //       </div>
+// //     </header>
+// //   );
+// // }
+// // import Link from "next/link";
+// // import MobileMenu from "./mobile-menu";
+
+// // export default function Header() {
+// //   return (
+// //     <header className="absolute w-full z-30">
+// //       <div className="max-w-6xl mx-auto px-4 sm:px-6">
+// //         <div className="flex items-center justify-between h-20">
+// //           {/* Site branding */}
+// //           <div className="shrink-0 mr-2 mb-2">
+// //             {/* Logo */}
+// //             <p>Yahav Mesika </p>
+// //           </div>
+
+// //           <div className="md:flex md:items-center md:justify-between">
+// //             {/* Social links */}
+// //             <ul className="flex  md:order-1 md:ml-4 md:mb-0">
+// //               <p className="mr-2">054-2522677</p>
+// //               <p>LinkedIn:</p>
+// //               <li className="ml-2">
+// //                 <Link href="https://www.linkedin.com/in/yahav-mesika/">
+// //                   <a className="flex justify-center items-center text-purple-600 bg-gray-800 hover:text-gray-100 hover:bg-purple-600 rounded-full transition duration-150 ease-in-out" aria-label="LinkedIn">
+// //                     <svg
+// //                       className="w-8 h-8 fill-current"
+// //                       viewBox="0 0 32 32"
+// //                       xmlns="http://www.w3.org/2000/svg"
+// //                     >
+// //                       <path d="M16 8.2c-4.4 0-8 3.6-8 8 0 3.5 2.3 6.5 5.5 7.6.4.1.5-.2.5-.4V22c-2.2.5-2.7-1-2.7-1-.4-.9-.9-1.2-.9-1.2-.7-.5.1-.5.1-.5.8.1 1.2.8 1.2.8.7 1.3 1.9.9 2.3.7.1-.5.3-.9.5-1.1-1.8-.2-3.6-.9-3.6-4 0-.9.3-1.6.8-2.1-.1-.2-.4-1 .1-2.1 0 0 .7-.2 2.2.8.6-.2 1.3-.3 2-.3s1.4.1 2 .3c1.5-1 2.2-.8 2.2-.8.4 1.1.2 1.9.1 2.1.5.6.8 1.3.8 2.1 0 3.1-1.9 3.7-3.7 3.9.3.4.6.9.6 1.6v2.2c0 .2.1.5.6.4 3.2-1.1 5.5-4.1 5.5-7.6-.1-4.4-3.7-8-8.1-8z" />
+// //                     </svg>
+// //                   </a>
+// //                 </Link>
+// //               </li>
+// //               <li className="ml-2">
+
+// //                 <p>Mail</p>
+// //                 <p> </p>
+// //                 <a href="mailto:yahavmesika@gmail.com" className="flex justify-center items-center text-purple-600 bg-gray-800 hover:text-gray-100 hover:bg-purple-600 rounded-full transition duration-150 ease-in-out" aria-label="Email">
+// //                   <svg xmlns="http://www.w3.org/2000/svg" className="w-8 h-8" viewBox="0 0 24 24" fill="currentColor">
+// //                     className="w-8 h-8 fill-current"
+// //                     viewBox="0 0 32 32"
+// //                     xmlns="http://www.w3.org/2000/svg"
+// //                     <path d="M21,5H3C1.346,5,0,6.346,0,8v8c0,1.654,1.346,3,3,3h18c1.654,0,3-1.346,3-3V8C24,6.346,22.654,5,21,5z M21,16.086   l-7.84-5.227c-0.165-0.109-0.36-0.109-0.525,0L3,16.086V8h18V16.086z"/>
+// //                     <path d="M20.19,8L12,13.757L3.81,8H20.19z M4.056,9L12,13.566L19.944,9H4.056z"/>
+// //                   </svg>
+// //                 </a>
+// //               </li>
+// //             </ul>
+// //           </div>
+// //         </div>
+// //       </div>
+// //     </header>
+// //   );
+// // }
+// import React from "react";
 
 // export default function Header() {
 //   return (
@@ -9,77 +102,23 @@
 //           {/* Site branding */}
 //           <div className="shrink-0 mr-2 mb-2">
 //             {/* Logo */}
-//             <p>Yahav Mesika </p>
+//             <p>Yahav Mesika</p>
 //           </div>
 
 //           <div className="md:flex md:items-center md:justify-between">
 //             {/* Social links */}
-//             <ul className="flex  md:order-1 md:ml-4 md:mb-0">
-//               <p className="mr-2"> 054-2522677</p>
-//               <p>Social:</p>
+//             <ul className="flex md:order-1 md:ml-4 md:mb-0">
+//               <p className="mr-2">054-2522677</p>
 //               <li className="ml-2">
-//                 <Link
-//                   href="https://www.linkedin.com/in/yahav-mesika/"
-//                   className="flex justify-center items-center text-purple-600 bg-gray-800 hover:text-gray-100 hover:bg-purple-600 rounded-full transition duration-150 ease-in-out"
-//                   aria-label="LinkedIn"
-//                 >
-//                   <svg
-//                     className="w-8 h-8 fill-current"
-//                     viewBox="0 0 32 32"
-//                     xmlns="http://www.w3.org/2000/svg"
-//                   >
+//                 <a href="https://www.linkedin.com/in/yahav-mesika/" className="flex justify-center items-center text-purple-600 bg-gray-800 hover:text-gray-100 hover:bg-purple-600 rounded-full transition duration-150 ease-in-out" aria-label="LinkedIn">
+//                   <svg className="w-8 h-8 fill-current" viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg">
 //                     <path d="M16 8.2c-4.4 0-8 3.6-8 8 0 3.5 2.3 6.5 5.5 7.6.4.1.5-.2.5-.4V22c-2.2.5-2.7-1-2.7-1-.4-.9-.9-1.2-.9-1.2-.7-.5.1-.5.1-.5.8.1 1.2.8 1.2.8.7 1.3 1.9.9 2.3.7.1-.5.3-.9.5-1.1-1.8-.2-3.6-.9-3.6-4 0-.9.3-1.6.8-2.1-.1-.2-.4-1 .1-2.1 0 0 .7-.2 2.2.8.6-.2 1.3-.3 2-.3s1.4.1 2 .3c1.5-1 2.2-.8 2.2-.8.4 1.1.2 1.9.1 2.1.5.6.8 1.3.8 2.1 0 3.1-1.9 3.7-3.7 3.9.3.4.6.9.6 1.6v2.2c0 .2.1.5.6.4 3.2-1.1 5.5-4.1 5.5-7.6-.1-4.4-3.7-8-8.1-8z" />
 //                   </svg>
-//                 </Link>
-//               </li>
-//             </ul>
-//           </div>
-//         </div>
-//       </div>
-//     </header>
-//   );
-// }
-// import Link from "next/link";
-// import MobileMenu from "./mobile-menu";
-
-// export default function Header() {
-//   return (
-//     <header className="absolute w-full z-30">
-//       <div className="max-w-6xl mx-auto px-4 sm:px-6">
-//         <div className="flex items-center justify-between h-20">
-//           {/* Site branding */}
-//           <div className="shrink-0 mr-2 mb-2">
-//             {/* Logo */}
-//             <p>Yahav Mesika </p>
-//           </div>
-
-//           <div className="md:flex md:items-center md:justify-between">
-//             {/* Social links */}
-//             <ul className="flex  md:order-1 md:ml-4 md:mb-0">
-//               <p className="mr-2">054-2522677</p>
-//               <p>LinkedIn:</p>
-//               <li className="ml-2">
-//                 <Link href="https://www.linkedin.com/in/yahav-mesika/">
-//                   <a className="flex justify-center items-center text-purple-600 bg-gray-800 hover:text-gray-100 hover:bg-purple-600 rounded-full transition duration-150 ease-in-out" aria-label="LinkedIn">
-//                     <svg
-//                       className="w-8 h-8 fill-current"
-//                       viewBox="0 0 32 32"
-//                       xmlns="http://www.w3.org/2000/svg"
-//                     >
-//                       <path d="M16 8.2c-4.4 0-8 3.6-8 8 0 3.5 2.3 6.5 5.5 7.6.4.1.5-.2.5-.4V22c-2.2.5-2.7-1-2.7-1-.4-.9-.9-1.2-.9-1.2-.7-.5.1-.5.1-.5.8.1 1.2.8 1.2.8.7 1.3 1.9.9 2.3.7.1-.5.3-.9.5-1.1-1.8-.2-3.6-.9-3.6-4 0-.9.3-1.6.8-2.1-.1-.2-.4-1 .1-2.1 0 0 .7-.2 2.2.8.6-.2 1.3-.3 2-.3s1.4.1 2 .3c1.5-1 2.2-.8 2.2-.8.4 1.1.2 1.9.1 2.1.5.6.8 1.3.8 2.1 0 3.1-1.9 3.7-3.7 3.9.3.4.6.9.6 1.6v2.2c0 .2.1.5.6.4 3.2-1.1 5.5-4.1 5.5-7.6-.1-4.4-3.7-8-8.1-8z" />
-//                     </svg>
-//                   </a>
-//                 </Link>
+//                 </a>
 //               </li>
 //               <li className="ml-2">
-
-//                 <p>Mail</p>
-//                 <p> </p>
 //                 <a href="mailto:yahavmesika@gmail.com" className="flex justify-center items-center text-purple-600 bg-gray-800 hover:text-gray-100 hover:bg-purple-600 rounded-full transition duration-150 ease-in-out" aria-label="Email">
 //                   <svg xmlns="http://www.w3.org/2000/svg" className="w-8 h-8" viewBox="0 0 24 24" fill="currentColor">
-//                     className="w-8 h-8 fill-current"
-//                     viewBox="0 0 32 32"
-//                     xmlns="http://www.w3.org/2000/svg"
 //                     <path d="M21,5H3C1.346,5,0,6.346,0,8v8c0,1.654,1.346,3,3,3h18c1.654,0,3-1.346,3-3V8C24,6.346,22.654,5,21,5z M21,16.086   l-7.84-5.227c-0.165-0.109-0.36-0.109-0.525,0L3,16.086V8h18V16.086z"/>
 //                     <path d="M20.19,8L12,13.757L3.81,8H20.19z M4.056,9L12,13.566L19.944,9H4.056z"/>
 //                   </svg>
@@ -92,6 +131,8 @@
 //     </header>
 //   );
 // }
+
+
 import React from "react";
 
 export default function Header() {
@@ -102,25 +143,26 @@ export default function Header() {
           {/* Site branding */}
           <div className="shrink-0 mr-2 mb-2">
             {/* Logo */}
-            <p>Yahav Mesika</p>
+            <p>Yahav Mesika </p>
           </div>
 
           <div className="md:flex md:items-center md:justify-between">
             {/* Social links */}
-            <ul className="flex md:order-1 md:ml-4 md:mb-0">
-              <p className="mr-2">054-2522677</p>
+            <ul className="flex  md:order-1 md:ml-4 md:mb-0">
+              <p className="mr-2"> 054-2522677</p>
+              <p>Social:</p>
               <li className="ml-2">
-                <a href="https://www.linkedin.com/in/yahav-mesika/" className="flex justify-center items-center text-purple-600 bg-gray-800 hover:text-gray-100 hover:bg-purple-600 rounded-full transition duration-150 ease-in-out" aria-label="LinkedIn">
-                  <svg className="w-8 h-8 fill-current" viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg">
+                <a
+                  href="https://www.linkedin.com/in/yahav-mesika/"
+                  className="flex justify-center items-center text-purple-600 bg-gray-800 hover:text-gray-100 hover:bg-purple-600 rounded-full transition duration-150 ease-in-out"
+                  aria-label="LinkedIn"
+                >
+                  <svg
+                    className="w-8 h-8 fill-current"
+                    viewBox="0 0 32 32"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
                     <path d="M16 8.2c-4.4 0-8 3.6-8 8 0 3.5 2.3 6.5 5.5 7.6.4.1.5-.2.5-.4V22c-2.2.5-2.7-1-2.7-1-.4-.9-.9-1.2-.9-1.2-.7-.5.1-.5.1-.5.8.1 1.2.8 1.2.8.7 1.3 1.9.9 2.3.7.1-.5.3-.9.5-1.1-1.8-.2-3.6-.9-3.6-4 0-.9.3-1.6.8-2.1-.1-.2-.4-1 .1-2.1 0 0 .7-.2 2.2.8.6-.2 1.3-.3 2-.3s1.4.1 2 .3c1.5-1 2.2-.8 2.2-.8.4 1.1.2 1.9.1 2.1.5.6.8 1.3.8 2.1 0 3.1-1.9 3.7-3.7 3.9.3.4.6.9.6 1.6v2.2c0 .2.1.5.6.4 3.2-1.1 5.5-4.1 5.5-7.6-.1-4.4-3.7-8-8.1-8z" />
-                  </svg>
-                </a>
-              </li>
-              <li className="ml-2">
-                <a href="mailto:yahavmesika@gmail.com" className="flex justify-center items-center text-purple-600 bg-gray-800 hover:text-gray-100 hover:bg-purple-600 rounded-full transition duration-150 ease-in-out" aria-label="Email">
-                  <svg xmlns="http://www.w3.org/2000/svg" className="w-8 h-8" viewBox="0 0 24 24" fill="currentColor">
-                    <path d="M21,5H3C1.346,5,0,6.346,0,8v8c0,1.654,1.346,3,3,3h18c1.654,0,3-1.346,3-3V8C24,6.346,22.654,5,21,5z M21,16.086   l-7.84-5.227c-0.165-0.109-0.36-0.109-0.525,0L3,16.086V8h18V16.086z"/>
-                    <path d="M20.19,8L12,13.757L3.81,8H20.19z M4.056,9L12,13.566L19.944,9H4.056z"/>
                   </svg>
                 </a>
               </li>
@@ -131,7 +173,5 @@ export default function Header() {
     </header>
   );
 }
-
-
 
 
