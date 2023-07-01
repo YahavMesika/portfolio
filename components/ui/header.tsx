c87f1d1aff504295574b7978325e6ca12342a3ae
YahavMesika <yahavmesika@gmail.com>
2023-07-01 12:25:30 +0300

    Update header.tsx

--- components/ui/header.tsx
+++ components/ui/header.tsx
@@ -57,21 +57,21 @@ export default function Header() {
             {/* Social links */}
             <ul className="flex  md:order-1 md:ml-4 md:mb-0">
               <p className="mr-2">054-2522677</p>
-              <p>Social:</p>
+              <p>LinkedIn:</p>
               <li className="ml-2">
-                <Link href="https://www.linkedin.com/in/yahav-mesika/">
-                  <a className="flex justify-center items-center text-purple-600 bg-gray-800 hover:text-gray-100 hover:bg-purple-600 rounded-full transition duration-150 ease-in-out" aria-label="LinkedIn">
-                    <svg
-                      className="w-8 h-8 fill-current"
-                      viewBox="0 0 32 32"
-                      xmlns="http://www.w3.org/2000/svg"
-                    >
-                      <path d="M16 8.2c-4.4 0-8 3.6-8 8 0 3.5 2.3 6.5 5.5 7.6.4.1.5-.2.5-.4V22c-2.2.5-2.7-1-2.7-1-.4-.9-.9-1.2-.9-1.2-.7-.5.1-.5.1-.5.8.1 1.2.8 1.2.8.7 1.3 1.9.9 2.3.7.1-.5.3-.9.5-1.1-1.8-.2-3.6-.9-3.6-4 0-.9.3-1.6.8-2.1-.1-.2-.4-1 .1-2.1 0 0 .7-.2 2.2.8.6-.2 1.3-.3 2-.3s1.4.1 2 .3c1.5-1 2.2-.8 2.2-.8.4 1.1.2 1.9.1 2.1.5.6.8 1.3.8 2.1 0 3.1-1.9 3.7-3.7 3.9.3.4.6.9.6 1.6v2.2c0 .2.1.5.6.4 3.2-1.1 5.5-4.1 5.5-7.6-.1-4.4-3.7-8-8.1-8z" />
-                    </svg>
-                  </a>
-                </Link>
-              </li>
+                  <Link href="https://www.linkedin.com/in/yahav-mesika/">
+                    <a className="flex justify-center items-center text-purple-600 bg-gray-800 hover:text-gray-100 hover:bg-purple-600 rounded-full transition duration-150 ease-in-out" aria-label="LinkedIn">
+                      <svg xmlns="http://www.w3.org/2000/svg" className="w-8 h-8" viewBox="0 0 24 24" fill="currentColor">
+                        <path d="M20.813 3.043h-17.626c-.608 0-1.187.244-1.613.678-.426.435-.664 1.023-.664 1.642v15.075c0 .62.238 1.207.664 1.642.426.435 1.005.678 1.613.678h17.626c.608 0 1.187-.244 1.613-.678.426-.435.664-1.022.664-1.642v-15.075c0-.619-.238-1.207-.664-1.642-.426-.434-1.005-.678-1.613-.678zm-17.626 17.629v-15.075h17.626v15.075h-17.626zm8.813-10.457h-1.476v-.676c0-.826-.614-1.514-1.4-1.514-.786 0-1.413.688-1.413 1.514v.676h-1.476v-.676c0-1.21.972-2.2 2.188-2.2 1.216 0 2.187.99 2.187 2.2v.676zm0 0"></path>
+                        <path d="M6.836 9.19h2.976v7.333h-2.976zm0 0"></path>
+                        <path d="M8.585 5.27c-.918 0-1.664.746-1.664 1.663s.746 1.664 1.664 1.664c.918 0 1.663-.746 1.663-1.664s-.745-1.663-1.663-1.663zm0 0"></path>
+                      </svg>
+                    </a>
+                  </Link>
+                </li>
+
               <li className="ml-2">
+              <p>Mail</p>
                 <a href="mailto:yahavmesika@gmail.com" className="flex justify-center items-center text-purple-600 bg-gray-800 hover:text-gray-100 hover:bg-purple-600 rounded-full transition duration-150 ease-in-out" aria-label="Email">
                   <svg
                     className="w-8 h-8 fill-current"
