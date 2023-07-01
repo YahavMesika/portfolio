0c0d0261facb58589806ce6f892d8e8c28f13b9e
YahavMesika <yahavmesika@gmail.com>
2023-07-01 12:56:55 +0300

    Update header.tsx

--- components/ui/header.tsx
+++ components/ui/header.tsx
@@ -133,7 +133,50 @@
 // }
 
 
-import React from "react";
+// import React from "react";
+
+// export default function Header() {
+//   return (
+//     <header className="absolute w-full z-30">
+//       <div className="max-w-6xl mx-auto px-4 sm:px-6">
+//         <div className="flex items-center justify-between h-20">
+//           {/* Site branding */}
+//           <div className="shrink-0 mr-2 mb-2">
+//             {/* Logo */}
+//             <p>Yahav Mesika </p>
+//           </div>
+
+//           <div className="md:flex md:items-center md:justify-between">
+//             {/* Social links */}
+//             <ul className="flex  md:order-1 md:ml-4 md:mb-0">
+//               <p className="mr-2"> 054-2522677</p>
+//               <p>Social:</p>
+//               <li className="ml-2">
+//                 <a
+//                   href="https://www.linkedin.com/in/yahav-mesika/"
+//                   className="flex justify-center items-center text-purple-600 bg-gray-800 hover:text-gray-100 hover:bg-purple-600 rounded-full transition duration-150 ease-in-out"
+//                   aria-label="LinkedIn"
+//                 >
+//                   <svg
+//                     className="w-8 h-8 fill-current"
+//                     viewBox="0 0 24 24"
+//                     xmlns="http://www.w3.org/2000/svg"
+//                   >
+//                       <path d="M22,3h-4.6c-0.4,0-0.8,0.3-0.8,0.7v4.7c0,0.4,0.3,0.8,0.7,0.8H22c0.4,0,0.8-0.3,0.8-0.7V3.7C22.8,3.3,22.5,3,22,3z M19.3,19.3h-3V9.2h3V19.3zM16.3,8.3c-1.1,0-2-0.9-2-2c0-1.1,0.9-2,2-2s2,0.9,2,2C18.3,7.4,17.4,8.3,16.3,8.3z M19.3,19.3h-3v-7.3c0-0.3-0.2-0.5-0.5-0.5H14c-0.3,0-0.5,0.2-0.5,0.5v7.3h-3c0,0,0-10.1,0-10.6c0-0.2,0-0.3-0.3-0.3h-3.6c-0.3,0-0.3,0.1-0.3,0.3v10.6h-3V9.2h2.7v1.3c0,0,0-0.9,1.1-0.9c1.1,0,1.2,0.9,1.2,0.9v-1.3h2.7v10.1h-3V19C3.1,19,3,19.3,3,19.3H0.7C0.3,19.3,0,19,0,18.7V3.7C0,3.3,0.3,3,0.7,3H4c0.4,0,0.7,0.3,0.7,0.7v1.6h3V3.7c0-0.4,0.3-0.7,0.7-0.7h3.6c0.4,0,0.7,0.3,0.7,0.7v14.6C7,19,6.7,19.3,6.3,19.3H3v1.6h3V19.3z"/>
+//                   </svg>
+//                 </a>
+//               </li>
+//             </ul>
+//           </div>
+//         </div>
+//       </div>
+//     </header>
+//   );
+// }
+npm install react-icons
+import { FaLinkedin } from 'react-icons/fa';
+import Link from "next/link";
+import MobileMenu from "./mobile-menu";
 
 export default function Header() {
   return (
@@ -152,19 +195,13 @@ export default function Header() {
               <p className="mr-2"> 054-2522677</p>
               <p>Social:</p>
               <li className="ml-2">
-                <a
+                <Link
                   href="https://www.linkedin.com/in/yahav-mesika/"
                   className="flex justify-center items-center text-purple-600 bg-gray-800 hover:text-gray-100 hover:bg-purple-600 rounded-full transition duration-150 ease-in-out"
                   aria-label="LinkedIn"
                 >
-                  <svg
-                    className="w-8 h-8 fill-current"
-                    viewBox="0 0 24 24"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                      <path d="M22,3h-4.6c-0.4,0-0.8,0.3-0.8,0.7v4.7c0,0.4,0.3,0.8,0.7,0.8H22c0.4,0,0.8-0.3,0.8-0.7V3.7C22.8,3.3,22.5,3,22,3z M19.3,19.3h-3V9.2h3V19.3zM16.3,8.3c-1.1,0-2-0.9-2-2c0-1.1,0.9-2,2-2s2,0.9,2,2C18.3,7.4,17.4,8.3,16.3,8.3z M19.3,19.3h-3v-7.3c0-0.3-0.2-0.5-0.5-0.5H14c-0.3,0-0.5,0.2-0.5,0.5v7.3h-3c0,0,0-10.1,0-10.6c0-0.2,0-0.3-0.3-0.3h-3.6c-0.3,0-0.3,0.1-0.3,0.3v10.6h-3V9.2h2.7v1.3c0,0,0-0.9,1.1-0.9c1.1,0,1.2,0.9,1.2,0.9v-1.3h2.7v10.1h-3V19C3.1,19,3,19.3,3,19.3H0.7C0.3,19.3,0,19,0,18.7V3.7C0,3.3,0.3,3,0.7,3H4c0.4,0,0.7,0.3,0.7,0.7v1.6h3V3.7c0-0.4,0.3-0.7,0.7-0.7h3.6c0.4,0,0.7,0.3,0.7,0.7v14.6C7,19,6.7,19.3,6.3,19.3H3v1.6h3V19.3z"/>
-                  </svg>
-                </a>
+                  <FaLinkedin />
+                </Link>
               </li>
             </ul>
           </div>
