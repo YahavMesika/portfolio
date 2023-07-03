37ef2600e3b3ccde6ca44a13a79af66d63479c8f
YahavMesika <yahavmesika@gmail.com>
2023-07-03 15:21:43 +0300

    aa

--- components/zigzag.tsx
+++ components/zigzag.tsx
@@ -74,31 +74,29 @@ export default function Zigzag() {
               {/* Image */}
               <div className="max-w-xl md:max-w-none md:w-full mx-auto md:col-span-5 lg:col-span-6 mb-8 md:mb-0 md:order-1" data-aos="fade-up">
               <a href="https://youtu.be/1FOgoble-Vs">
-                <Image className="max-w-full mx-auto md:max-w-none h-auto" src={FeatImage02} width={540} height={405} alt="git-finder"  />
+                <Image className="max-w-full mx-auto md:max-w-none h-auto" src={FeatImage05} width={540} height={405} alt="git-finder"  />
                 <p className='text-center text-purple-600 hover:underline'>Click on the image to see the project in detailed</p>
                 </a>
               </div>
-      
-
               {/* Content */}
               <div className="max-w-xl md:max-w-none md:w-full mx-auto md:col-span-7 lg:col-span-6" data-aos="fade-right">
                 <div className="md:pr-4 lg:pr-12 xl:pr-16">
                   <div className="font-architects-daughter text-xl text-purple-600 mb-2"></div>
-                  <h3 className="h3 mb-3">Github Finder
+                  <h3 className="h3 mb-3">BI project
                   </h3>
-                  <p className="text-xl text-gray-400 mb-4">Use github api to search user and dispaly all their badge and repos</p>
+                  <p className="text-xl text-gray-400 mb-4">Analyzed football data to identify key performance indicators and insights for team managers and coaches.</br> Investigated the relationship between fan size and goal scoring.</p>
                   <ul className="text-lg text-gray-400 -mb-2">
                     <li className="flex items-center mb-2">
                       <svg className="w-3 h-3 fill-current text-green-500 mr-2 shrink-0" viewBox="0 0 12 12" xmlns="http://www.w3.org/2000/svg">
                         <path d="M10.28 2.28L3.989 8.575 1.695 6.28A1 1 0 00.28 7.695l3 3a1 1 0 001.414 0l7-7A1 1 0 0010.28 2.28z" />
                       </svg>
-                      <span>Find users by name</span>
+                      <span>Pre-processing using by python</span>
                     </li>
                     <li className="flex items-center mb-2">
                       <svg className="w-3 h-3 fill-current text-green-500 mr-2 shrink-0" viewBox="0 0 12 12" xmlns="http://www.w3.org/2000/svg">
                         <path d="M10.28 2.28L3.989 8.575 1.695 6.28A1 1 0 00.28 7.695l3 3a1 1 0 001.414 0l7-7A1 1 0 0010.28 2.28z" />
                       </svg>
-                      <span>See users work in a more convenient way </span>
+                      <span>Buil</span>
                     </li>
                     <li className="flex items-center">
                       <svg className="w-3 h-3 fill-current text-green-500 mr-2 shrink-0" viewBox="0 0 12 12" xmlns="http://www.w3.org/2000/svg">
