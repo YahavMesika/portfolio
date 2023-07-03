7441f0c7cd4b7a7bbe26bdd287a7dd2105c2062c
YahavMesika <yahavmesika@gmail.com>
2023-07-03 17:32:31 +0300

    as

--- components/features.tsx
+++ components/features.tsx
@@ -1,11 +1,11 @@
 export default function Features() {
-  const full_Name = "Ori Keysar"
+  const full_Name = "Yahav Mesika"
   return (
     <section>
       <div className="max-w-6xl mx-auto px-4 sm:px-6">
         <div className="py-12 md:py-20">
 
-          {/* Section header */}
+          {/* Section header
           <div className="max-w-3xl mx-auto text-center pb-12 md:pb-20">
             <h2 className="h2 mb-4">Nice to meet you!</h2>
             <p className="text-xl text-gray-400">Thank you for visiting my portfolio website.<br/> 
@@ -14,21 +14,21 @@ export default function Features() {
             Through my analytical skills, fast learning abilities, and dedication, I aim to make a positive impact in the field.<br/>
              Feel free to explore my projects and skills, and don't hesitate to reach out to discuss potential collaborations or opportunities.<br/> <br/>
              Let's connect and create something remarkable together.<br/></p>
-          </div>
+          </div> */}
 
           {/* Items */}
           <div className="max-w-sm mx-auto grid gap-8 md:grid-cols-2 lg:grid-cols-3 lg:gap-16 items-start md:max-w-2xl lg:max-w-none" data-aos-id-blocks>
 
-            {/* 1st item */}
+            {/* 1st item*/}
             <div className="relative flex flex-col items-center" data-aos="fade-up" data-aos-anchor="[data-aos-id-blocks]">
               <svg className="w-16 h-16 mb-4" viewBox="0 0 64 64" xmlns="http://www.w3.org/2000/svg">
                 <rect className="fill-current text-purple-600" width="64" height="64" rx="32" />
                 <path className="stroke-current text-purple-100" d="M30 39.313l-4.18 2.197L27 34.628l-5-4.874 6.91-1.004L32 22.49l3.09 6.26L42 29.754l-3 2.924" strokeLinecap="square" strokeWidth="2" fill="none" fillRule="evenodd" />
                 <path className="stroke-current text-purple-300" d="M43 42h-9M43 37h-9" strokeLinecap="square" strokeWidth="2" />
               </svg>
-              <h4 className="h4 mb-4  text-center">	Softwares</h4>
-              <p className="text-lg text-gray-400 text-center">MSQL, Spss, Ms Project, Knime, Qlik, Tableau, Arena, Word, PowerPoint, Excel</p>
-            </div>
+              <h4 className="h4 mb-4  text-center">Achievements</h4>
+              <p className="text-lg text-gray-400 text-center">2022 - ML Hackathon winner <br></br> 2023 - ML Hackathon winner </p>
+            </div> 
 
             {/* 2nd item */}
             <div className="relative flex flex-col items-center " data-aos="fade-up" data-aos-delay="100" data-aos-anchor="[data-aos-id-blocks]">
@@ -37,11 +37,11 @@ export default function Features() {
                 <path className="stroke-current text-purple-100" strokeWidth="2" strokeLinecap="square" d="M21 23h22v18H21z" fill="none" fillRule="evenodd" />
                 <path className="stroke-current text-purple-300" d="M26 28h12M26 32h12M26 36h5" strokeWidth="2" strokeLinecap="square" />
               </svg>
-              <h4 className="h4 mb-4 text-center">programming languages/technologies </h4>
-              <p className="text-lg text-gray-400 text-center">C++, C#, .NET, ASP.NET, HTML5, CSS, JavaScript, TypeScript, jQuery, Angular, TailWind.css, React.js, Advence SQL , FirebaseDB,No-Sql Database</p>
+              <h4 className="h4 mb-4 text-center">Skills</h4>
+              <p className="text-lg text-gray-400 text-center">Python, Tableau, Qlik-Sense, Excel,Machine Learning, NoSQL-MongoDB</p>
             </div>
 
-            {/* 3rd item */}
+            {/* 3rd item
             <div className="relative flex flex-col items-center" data-aos="fade-up" data-aos-delay="200" data-aos-anchor="[data-aos-id-blocks]">
               <svg className="w-16 h-16 mb-4" viewBox="0 0 64 64" xmlns="http://www.w3.org/2000/svg">
                 <rect className="fill-current text-purple-600" width="64" height="64" rx="32" />
@@ -54,7 +54,7 @@ export default function Features() {
               <h4 className="h4 mb-4  text-center">information systems analysis and characterization</h4>
               <p className="text-lg text-gray-400 text-center">ERD, BPMN, USE CASE, CLASS MODEL, SEQUENCE MODEL, State 
               Chart Modal</p>
-            </div>
+            </div> */}
 {/* 
             4th item
             <div className="relative flex flex-col items-center" data-aos="fade-up" data-aos-delay="300" data-aos-anchor="[data-aos-id-blocks]">
