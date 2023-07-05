c9d08b71bc388f3d9503aaa1874e509287613c97
YahavMesika <yahavmesika@gmail.com>
2023-07-05 14:32:30 +0300

    Update features.tsx

--- components/features.tsx
+++ components/features.tsx
@@ -31,19 +31,17 @@ export default function Features() {
             </div>
 
             {/* 2nd item */}
-            <div className="relative flex flex-col items-center" data-aos="fade-up" data-aos-delay="100" data-aos-anchor="[data-aos-id-blocks]">
-             <svg className="w-16 h-16 mb-4 ml-4" viewBox="0 0 64 64" xmlns="http://www.w3.org/2000/svg">
+            {/* <div className="relative flex flex-col items-center " data-aos="fade-up" data-aos-delay="100" data-aos-anchor="[data-aos-id-blocks]">
+              <svg className="w-16 h-16 mb-4" viewBox="0 0 64 64" xmlns="http://www.w3.org/2000/svg">
                 <circle className="fill-current text-purple-600" cx="32" cy="32" r="32" />
                 <path className="stroke-current text-purple-100" strokeWidth="2" strokeLinecap="square" d="M21 23h22v18H21z" fill="none" fillRule="evenodd" />
                 <path className="stroke-current text-purple-300" d="M26 28h12M26 32h12M26 36h5" strokeWidth="2" strokeLinecap="square" />
               </svg>
               <h4 className="h4 mb-4 text-center">Skills</h4>
-              <p className="text-lg text-gray-400 text-center ml-4">Python, Tableau, Qlik-Sense, Excel, Machine Learning, Neural Networks, NoSQL-MongoDB</p>
-            </div>
+              <p className="text-lg text-gray-400 text-center">Python, Tableau, Qlik-Sense, Excel, Machine Learning, Neural Networks, NoSQL-MongoDB</p>
+            </div> */}
 
-
-
-            {/* 3rd item
+            {/* 3rd item*/}
             <div className="relative flex flex-col items-center" data-aos="fade-up" data-aos-delay="200" data-aos-anchor="[data-aos-id-blocks]">
               <svg className="w-16 h-16 mb-4" viewBox="0 0 64 64" xmlns="http://www.w3.org/2000/svg">
                 <rect className="fill-current text-purple-600" width="64" height="64" rx="32" />
@@ -53,10 +51,9 @@ export default function Features() {
                   <circle className="stroke-current text-purple-100" cx="11" cy="11" r="11" />
                 </g>
               </svg>
-              <h4 className="h4 mb-4  text-center">information systems analysis and characterization</h4>
-              <p className="text-lg text-gray-400 text-center">ERD, BPMN, USE CASE, CLASS MODEL, SEQUENCE MODEL, State 
-              Chart Modal</p> */}
-            {/* </div> */}
+              <h4 className="h4 mb-4  text-center">Skills</h4>
+              <p className="text-lg text-gray-400 text-center">Python, Tableau, Qlik-Sense, Excel, Machine Learning, Neural Networks, NoSQL-MongoDB</p>
+            </div> 
 {/* 
             4th item
             <div className="relative flex flex-col items-center" data-aos="fade-up" data-aos-delay="300" data-aos-anchor="[data-aos-id-blocks]">
