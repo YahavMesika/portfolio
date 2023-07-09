4d33d30ef6f056f0b9bfb9df846419508b71afb5
YahavMesika <yahavmesika@gmail.com>
2023-07-09 19:02:34 +0300

    Update zigzag.tsx

--- components/zigzag.tsx
+++ components/zigzag.tsx
@@ -122,8 +122,8 @@ export default function Zigzag() {
               {/* Content */}
               <div className="max-w-xl md:max-w-none md:w-full mx-auto md:col-span-7 lg:col-span-6" data-aos="fade-right">
                 <div className="md:pr-4 lg:pr-12 xl:pr-16">
-                  <div className="font-architects-daughter text-xl text-purple-600 mb-2"></div>
-                  <h3 className="h3 mb-3">SQL Server project App</h3>
+                  <div className="font-architects-daughter text-xl text-purple-600 mb-2">SQL project</div>
+                  <h3 className="h3 mb-3">Developed in SQL Server Managment Studio</h3>
                   <p className="text-xl text-gray-400 mb-4"> I have used SQL to find certain segments in the database that will be used for insights and will influence decision-making.</p>
                   <ul className="text-lg text-gray-400 -mb-2">
                     <li className="flex items-center mb-2">
