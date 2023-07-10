7c3eda3561045ba8cc453859abb68dd445a88398
YahavMesika <yahavmesika@gmail.com>
2023-07-10 17:17:34 +0300

    Update zigzag.tsx

--- components/zigzag.tsx
+++ components/zigzag.tsx
@@ -23,39 +23,53 @@ export default function Zigzag() {
           {/* Items */}
           <div className="grid gap-20">
 
-            {/* 1st item */}
-            <div className="md:grid md:grid-cols-12 md:gap-6 items-center">
+          {   /* 1nd item */}
+              <div className="md:grid md:grid-cols-12 md:gap-6 items-center">
               {/* Image */}
               <div className="max-w-xl md:max-w-none md:w-full mx-auto md:col-span-5 lg:col-span-6 mb-8 md:mb-0 md:order-1" data-aos="fade-up">
-              <a href="https://github.com/YahavMesika/NoSQL-project/blob/main/MongoDB_script.txt">
-                <Image className="max-w-full mx-auto md:max-w-none h-auto" src={FeatImage01} width={540} height={405} alt="ReGroup"  />
-                <p className='text-center text-purple-600 hover:underline'>Click on the image to see the script</p>
+              <a href="https://github.com/YahavMesika/FinalProject/blob/main/FinalProjectNotebook%20(2).ipynb">
+                {/* <Image className="max-w-full mx-auto md:max-w-none h-auto" src={FeatImage05} width={540} height={202} alt="BI project"  /> */}
+                <Image className="max-w-full mx-auto md:max-w-none h-auto" src={FeatImage05a} width={540} height={202} alt="BI project"  />
+                {/* <Image className="max-w-full mx-auto md:max-w-none h-auto" src={FeatImage05b} width={540} height={202} alt="BI project"  /> */}
+                <p className='text-center text-purple-600 hover:underline'>Click on the image to see the project in detailed</p>
                 </a>
               </div>
               {/* Content */}
               <div className="max-w-xl md:max-w-none md:w-full mx-auto md:col-span-7 lg:col-span-6" data-aos="fade-right">
                 <div className="md:pr-4 lg:pr-12 xl:pr-16">
-                  <div className="font-architects-daughter text-xl text-purple-600 mb-4">NoSQL project </div>
-                  <h3 className="h3 mb-3"> Developed in MongoDB tool</h3>
-                  <p className="text-xl text-gray-400 mb-4">Creating a database, Writing functions and queries.</p>
+                  <div className="font-architects-daughter text-xl text-purple-600 mb-2"></div>
+                  <h3 className="h3 mb-3">Final project</h3>
+                  <p className="text-xl text-gray-400 mb-4">My final project deals with the Alexander River estuary, the goal of the project is to develop a model using machine learning that will predict the concentration of bacteria in the estuary.</p>
                   <ul className="text-lg text-gray-400 -mb-2">
                     <li className="flex items-center mb-2">
                       <svg className="w-3 h-3 fill-current text-green-500 mr-2 shrink-0" viewBox="0 0 12 12" xmlns="http://www.w3.org/2000/svg">
                         <path d="M10.28 2.28L3.989 8.575 1.695 6.28A1 1 0 00.28 7.695l3 3a1 1 0 001.414 0l7-7A1 1 0 0010.28 2.28z" />
                       </svg>
-                      <span> Created a football database </span>
+                      <span>Meeting with the estuary researchers and getting to know the data </span>
                     </li>
                     <li className="flex items-center mb-2">
                       <svg className="w-3 h-3 fill-current text-green-500 mr-2 shrink-0" viewBox="0 0 12 12" xmlns="http://www.w3.org/2000/svg">
                         <path d="M10.28 2.28L3.989 8.575 1.695 6.28A1 1 0 00.28 7.695l3 3a1 1 0 001.414 0l7-7A1 1 0 0010.28 2.28z" />
                       </svg>
-                      <span> Developed and implemented various database functions to handle data manipulation tasks. </span>
+                      <span>EDA process on the database </span>
+                    </li>
+                    <li className="flex items-center mb-2">
+                      <svg className="w-3 h-3 fill-current text-green-500 mr-2 shrink-0" viewBox="0 0 12 12" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M10.28 2.28L3.989 8.575 1.695 6.28A1 1 0 00.28 7.695l3 3a1 1 0 001.414 0l7-7A1 1 0 0010.28 2.28z" />
+                      </svg>
+                      <span>Cross-referencing the primary database with secondary databases </span>
                     </li>
                     <li className="flex items-center">
                       <svg className="w-3 h-3 fill-current text-green-500 mr-2 shrink-0" viewBox="0 0 12 12" xmlns="http://www.w3.org/2000/svg">
                         <path d="M10.28 2.28L3.989 8.575 1.695 6.28A1 1 0 00.28 7.695l3 3a1 1 0 001.414 0l7-7A1 1 0 0010.28 2.28z" />
                       </svg>
-                      <span>Created custom queries to extract specific data subsets, enabling detailed analysis and generating meaningful insights for decision-making purposes.</span>
+                      <span>Descriptive statistics and statistical tests</span>
+                    </li>
+                    <li className="flex items-center">
+                      <svg className="w-3 h-3 fill-current text-green-500 mr-2 shrink-0" viewBox="0 0 12 12" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M10.28 2.28L3.989 8.575 1.695 6.28A1 1 0 00.28 7.695l3 3a1 1 0 001.414 0l7-7A1 1 0 0010.28 2.28z" />
+                      </svg>
+                      <span>Models and GUI</span>
                     </li>
                     {/* delete from here */}
                     {/* <li className="flex items-center">
@@ -201,54 +215,40 @@ export default function Zigzag() {
               </div>
             </div>
 
-            {   /* 5nd item */}
-              <div className="md:grid md:grid-cols-12 md:gap-6 items-center">
-              {/* Image */}
-              <div className="max-w-xl md:max-w-none md:w-full mx-auto md:col-span-5 lg:col-span-6 mb-8 md:mb-0 md:order-1" data-aos="fade-up">
-              <a href="https://github.com/YahavMesika/FinalProject/blob/main/FinalProjectNotebook%20(2).ipynb">
-                {/* <Image className="max-w-full mx-auto md:max-w-none h-auto" src={FeatImage05} width={540} height={202} alt="BI project"  /> */}
-                <Image className="max-w-full mx-auto md:max-w-none h-auto" src={FeatImage05a} width={540} height={202} alt="BI project"  />
-                {/* <Image className="max-w-full mx-auto md:max-w-none h-auto" src={FeatImage05b} width={540} height={202} alt="BI project"  /> */}
-                <p className='text-center text-purple-600 hover:underline'>Click on the image to see the project in detailed</p>
-                </a>
-              </div>
-              {/* Content */}
-              <div className="max-w-xl md:max-w-none md:w-full mx-auto md:col-span-7 lg:col-span-6" data-aos="fade-right">
-                <div className="md:pr-4 lg:pr-12 xl:pr-16">
-                  <div className="font-architects-daughter text-xl text-purple-600 mb-2"></div>
-                  <h3 className="h3 mb-3">Final project</h3>
-                  <p className="text-xl text-gray-400 mb-4">My final project deals with the Alexander River estuary, the goal of the project is to develop a model using machine learning that will predict the concentration of bacteria in the estuary.</p>
-                  <ul className="text-lg text-gray-400 -mb-2">
-                    <li className="flex items-center mb-2">
-                      <svg className="w-3 h-3 fill-current text-green-500 mr-2 shrink-0" viewBox="0 0 12 12" xmlns="http://www.w3.org/2000/svg">
-                        <path d="M10.28 2.28L3.989 8.575 1.695 6.28A1 1 0 00.28 7.695l3 3a1 1 0 001.414 0l7-7A1 1 0 0010.28 2.28z" />
-                      </svg>
-                      <span>Meeting with the estuary researchers and getting to know the data </span>
-                    </li>
-                    <li className="flex items-center mb-2">
-                      <svg className="w-3 h-3 fill-current text-green-500 mr-2 shrink-0" viewBox="0 0 12 12" xmlns="http://www.w3.org/2000/svg">
-                        <path d="M10.28 2.28L3.989 8.575 1.695 6.28A1 1 0 00.28 7.695l3 3a1 1 0 001.414 0l7-7A1 1 0 0010.28 2.28z" />
-                      </svg>
-                      <span>EDA process on the database </span>
-                    </li>
-                    <li className="flex items-center mb-2">
-                      <svg className="w-3 h-3 fill-current text-green-500 mr-2 shrink-0" viewBox="0 0 12 12" xmlns="http://www.w3.org/2000/svg">
-                        <path d="M10.28 2.28L3.989 8.575 1.695 6.28A1 1 0 00.28 7.695l3 3a1 1 0 001.414 0l7-7A1 1 0 0010.28 2.28z" />
-                      </svg>
-                      <span>Cross-referencing the primary database with secondary databases </span>
-                    </li>
-                    <li className="flex items-center">
-                      <svg className="w-3 h-3 fill-current text-green-500 mr-2 shrink-0" viewBox="0 0 12 12" xmlns="http://www.w3.org/2000/svg">
-                        <path d="M10.28 2.28L3.989 8.575 1.695 6.28A1 1 0 00.28 7.695l3 3a1 1 0 001.414 0l7-7A1 1 0 0010.28 2.28z" />
-                      </svg>
-                      <span>Descriptive statistics and statistical tests</span>
-                    </li>
-                    <li className="flex items-center">
-                      <svg className="w-3 h-3 fill-current text-green-500 mr-2 shrink-0" viewBox="0 0 12 12" xmlns="http://www.w3.org/2000/svg">
-                        <path d="M10.28 2.28L3.989 8.575 1.695 6.28A1 1 0 00.28 7.695l3 3a1 1 0 001.414 0l7-7A1 1 0 0010.28 2.28z" />
-                      </svg>
-                      <span>Models and GUI</span>
-                    </li>
+            {/* 1st item */}
+            <div className="md:grid md:grid-cols-12 md:gap-6 items-center">
+            {/* Image */}
+                      <div className="max-w-xl md:max-w-none md:w-full mx-auto md:col-span-5 lg:col-span-6 mb-8 md:mb-0 md:order-1" data-aos="fade-up">
+                      <a href="https://github.com/YahavMesika/NoSQL-project/blob/main/MongoDB_script.txt">
+                        <Image className="max-w-full mx-auto md:max-w-none h-auto" src={FeatImage01} width={540} height={405} alt="ReGroup"  />
+                        <p className='text-center text-purple-600 hover:underline'>Click on the image to see the script</p>
+                        </a>
+                      </div>
+                      {/* Content */}
+                      <div className="max-w-xl md:max-w-none md:w-full mx-auto md:col-span-7 lg:col-span-6" data-aos="fade-right">
+                        <div className="md:pr-4 lg:pr-12 xl:pr-16">
+                          <div className="font-architects-daughter text-xl text-purple-600 mb-4">NoSQL project </div>
+                          <h3 className="h3 mb-3"> Developed in MongoDB tool</h3>
+                          <p className="text-xl text-gray-400 mb-4">Creating a database, Writing functions and queries.</p>
+                          <ul className="text-lg text-gray-400 -mb-2">
+                            <li className="flex items-center mb-2">
+                              <svg className="w-3 h-3 fill-current text-green-500 mr-2 shrink-0" viewBox="0 0 12 12" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M10.28 2.28L3.989 8.575 1.695 6.28A1 1 0 00.28 7.695l3 3a1 1 0 001.414 0l7-7A1 1 0 0010.28 2.28z" />
+                              </svg>
+                              <span> Created a football database </span>
+                            </li>
+                            <li className="flex items-center mb-2">
+                              <svg className="w-3 h-3 fill-current text-green-500 mr-2 shrink-0" viewBox="0 0 12 12" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M10.28 2.28L3.989 8.575 1.695 6.28A1 1 0 00.28 7.695l3 3a1 1 0 001.414 0l7-7A1 1 0 0010.28 2.28z" />
+                              </svg>
+                              <span> Developed and implemented various database functions to handle data manipulation tasks. </span>
+                            </li>
+                            <li className="flex items-center">
+                              <svg className="w-3 h-3 fill-current text-green-500 mr-2 shrink-0" viewBox="0 0 12 12" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M10.28 2.28L3.989 8.575 1.695 6.28A1 1 0 00.28 7.695l3 3a1 1 0 001.414 0l7-7A1 1 0 0010.28 2.28z" />
+                              </svg>
+                              <span>Created custom queries to extract specific data subsets, enabling detailed analysis and generating meaningful insights for decision-making purposes.</span>
+                            </li>
                   </ul>
                 </div>
               </div>
